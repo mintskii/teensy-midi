7501d5ba8e05197ea354c8abb3a6ab2d256d446c
mintskii <mintskii@gmail.com>
2015-02-22 15:19:13 +0000

    Tried (and failed) with code from filib
    Code works fine when #define analogInputs is 1 - output is stable, but
    when set to anything higher it constantly chucks out readings which
    makes it impossible to MIDI-learn from an app, and the 2nd pot doesn't
    work anyway

---
+++ filib_teensy_midi_original_code/filib_teensy_midi_original_code.ino
@@ -0,0 +1,32 @@
+// define how many pots are active up to number of available analog inputs
+#define analogInputs 2
+// make arrays for input values and lagged input values
+int inputAnalog[analogInputs];
+int iAlag[analogInputs];
+// make array of cc values
+int ccValue[analogInputs];
+// index variable for loop
+int i;
+
+void setup() {
+  // MIDI rate
+  Serial.begin(31250);
+}
+
+void loop() {
+  // loop trough active inputs
+  for (i=0;i<analogInputs;i++){
+    // read current value at i-th input
+    inputAnalog[i] = analogRead(i);
+    // if magnitude of difference is 8 or more...
+    if (abs(inputAnalog[i] - iAlag[i]) > 7){
+      // calc the CC value based on the raw value
+      ccValue[i] = inputAnalog[i]/8;
+      // send the MIDI
+      usbMIDI.sendControlChange(i, ccValue[i], 1);
+      // set raw reading to lagged array for next comparison
+      iAlag[i] = inputAnalog[i];
+    }
+  delay(5); // limits MIDI messages to reasonable number
+  }
+}
